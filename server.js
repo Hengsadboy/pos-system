224ba34005f507c262b9e46990a7ee58f34c6826
Harik-Valentine <henglop1999@gmail.com>
2026-07-13 22:05:33 +0700

    Allow Express server to run on port 3000 behind Nginx SSL reverse proxy

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require('express');
 const path = require('path');
 const sqlite3 = require('sqlite3').verbose();
 const app = express();
-const PORT = 80;
+const PORT = process.env.PORT || 3000;
 
 const DB_FILE = path.join(__dirname, 'database.db');
 
