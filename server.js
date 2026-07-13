78e56bd0c0cfbaf6190892ebef80da943cf693f8
Harik-Valentine <henglop1999@gmail.com>
2026-07-13 21:44:13 +0700

    Set Express server port to 80 for direct public HTTP access

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require('express');
 const fs = require('fs');
 const path = require('path');
 const app = express();
-const PORT = 3000;
+const PORT = 80;
 
 const DB_FILE = path.join(__dirname, 'database.json');
 
